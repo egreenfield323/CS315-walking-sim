5f4475bc3c73ccecf70dbaa6b87986c6f0f0e3c6
Evan Greenfield <evangreenfield@live.com>
2024-10-23 17:38:14 -0400

    Update player.gd

--- cs315-walking-sim/scripts/player.gd
+++ cs315-walking-sim/scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody3D
 
 @export var speed = 10
-var mouse_sensitivity = 0.03
+var mouse_sensitivity = .005
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector3.ZERO
-
+	velocity = Vector3(0, 0, 0)
+	
 	if Input.is_action_pressed("W"):
 		velocity = get_global_transform().basis.z * -speed
 		
